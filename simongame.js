e0be704fb20d4e184596573cb7c9a1d4be260cec
Naja <113134845+najamoe@users.noreply.github.com>
2024-04-03 13:10:48 +0000

    added let and const for js

---
+++ simongame.js
@@ -0,0 +1,27 @@
+"use Strict";
+
+let order = []; //Limited scope, only applied to where it is defined - keeps track on the order of the lights flash
+let playerOrder = []; //Order of the players pressing
+let flash; //Integer of numbers of flashes in game
+let turn; // Keeps track of which turn the game is on 
+let good; //True or false, whether the player hits the right colors
+let compTurn; //boolean, keep track of its the computer or players turn
+let intervalId; // 
+let strict = false; //check if strict button is enabled
+let noise = true; 
+let on = false; //If programme has been turned on (start turned off)
+let win; 
+
+//#css selector for all html-elements we interact with
+const turnCounter = document.querySelector("#turn"); 
+const topLeft = document.querySelector("#topleft");
+const topRight = document.querySelector("#topright");
+const bottomLeft = document.querySelector("#bottomLeft");
+const bottomRight = document.querySelector("#bottomRight");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
+
+
+
+
